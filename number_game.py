9305383d5a0b9498d3e724e6e30cabc7007c0b62
Shaun Carrigan <smcarrigan@gmail.com>
2015-09-02 22:49:53 -0700

    Random changes

--- number_game.py
+++ number_game.py
@@ -1,5 +1,13 @@
 import random
 
+class Bank(object):
+	def __init__(self, inital_deposit):
+		self.total_amount = inital_deposit
+	def deposit(amount):
+		self.total_amount += amount
+	def withdrawal(amount):
+		self.total_amount -= amount
+
 def check_value(guess, correct_value):
 	if guess == correct_value:
 		return True
@@ -17,6 +25,9 @@ def get_input():
 		'num_of_tries': num_of_tries
 		}
 
+def calculate_inputs(input_dict, bank):
+	difference = input_dict['max_random_value'] - input_dict['min_random_value']
+
 def create_list(input_dict):
 	return [random.randint(input_dict['min_random_value'],input_dict['max_random_value'])
 		       for x in range(1,input_dict['num_of_list_values']+1)]
@@ -29,9 +40,9 @@ def game(random_list, input_dict):
 
 	active_list = ['x' for x in range(0,len(random_list))]
 	while(True):
-		print(active_list)
-		print("Number of tries left: %d" % num_of_tries)
 		try:
+			print(active_list)
+			print("Number of tries left: %d" % num_of_tries)
 			input_value = int(input("Enter guess between " + min_value + " and " + max_value + ": "))
 			if input_value < int(min_value) or input_value > int(max_value):
 				raise ValueError
@@ -53,13 +64,18 @@ def game(random_list, input_dict):
 					print ("Guess lower")
 		except ValueError:
 			print("Invalid input.  Integer value must be between " + min_value + " and " + max_value + ".")
-		except KeyboardInterrupt:
-			print("Good Bye!")
-			exit()
 def main():
+	print("Welcome to the number guessing game.  You start with $100 and the goal is to try to earn $100k."
+	bank = Bank(100.00)
+	print("$%.2f" % bank.total_amount)
 	input_dict = get_input()
+	calculate_inputs(input_dict,bank)
 	random_list = create_list(input_dict)
 	game(random_list, input_dict)
 
 if __name__ == "__main__":
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("\nGood Bye!")
+		exit()
